d197665d7e5eb392e25848e73ed12d33724e8d20
blockenters <blockenters@gmail.com>
2022-05-23 16:30:40 +0900

    Update app_ml.py

--- app_ml.py
+++ app_ml.py
@@ -1,9 +1,13 @@
 import streamlit as st 
 
 import joblib
+import numpy as np
+
+
 
 def run_ml() :
     st.subheader('자동차 구매 가능 금액 예측')
+
 
     # 예측하기 위해서 필요한 파일들을 불러와야 된다.
     # 이 예에서는, 인공지능파일, X 스케일러 파일, y 스케일러파일
@@ -17,7 +21,6 @@ def run_ml() :
     # 만드세요. 
 
     gender = st.radio('성별 입력', ['남자', '여자'])
-
     if gender == '여자' :
         gender = 0
     else :
@@ -28,6 +31,24 @@ def run_ml() :
     debt = st.number_input('카드빚 입력', 0)
     worth = st.number_input('자산 입력', 0)
 
+    if st.button('자동차 구매 금액 예측') : 
+        # 1. 신규 고객의 정보를 넘파이 어레이로 만들어준다.
+        new_data = np.array([gender, age, salary, debt, worth])
+
+        # 2. 학습할때 사용한 X 의 피처 스케일러를 이용해서, 피처스케일링한다.
+        # 먼저, 데이터를 2차원으로 만들어 준다.
+        new_data = new_data.reshape(1, 5)
+        new_data = scaler_X.transform(new_data)
+
+        # 3. 인공지능에게 예측해달라고 한다.
+        y_pred = regressor.predict(new_data)
+
+        # 4. 예측한 값을, 원상복구 시킨다.
+        y_pred = scaler_y.inverse_transform(y_pred)
+
+        y_pred = round(y_pred[0,0])
+
+        st.write( '이 사람의 구매 가능 금액은 '+ str(y_pred)  +'달라입니다.')
 
 
 
